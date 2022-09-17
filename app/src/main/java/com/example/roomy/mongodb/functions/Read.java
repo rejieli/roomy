ef06e8eb1d5eef49db0e68df1b2f945a73b04bab
Fan-Yang <fan.yang284@gmail.com>
2022-09-17 13:25:25 -0400

    update gradle min API level, and CRD (early commit to try and pull changes)

--- app/src/main/java/com/example/roomy/mongodb/functions/Read.java
+++ app/src/main/java/com/example/roomy/mongodb/functions/Read.java
@@ -1,2 +1,74 @@
-package com.example.roomy.mongodb.functions;public class Read {
+package com.example.roomy.mongodb.functions;
+
+import com.mongodb.client.*;
+import com.example.roomy.mongodb.models.UserProfile;
+
+import org.bson.Document;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
+import static com.mongodb.client.model.Filters.*;
+import static com.mongodb.client.model.Projections.*;
+import static com.mongodb.client.model.Sorts.descending;
+
+public class Read {
+
+    public static void main(String[] args) {
+        try (MongoClient mongoClient = MongoClients.create(System.getProperty("mongodb.uri"))) {
+            MongoDatabase usersDB = mongoClient.getDatabase("users");
+            MongoCollection<Document> profilesCollection = usersDB.getCollection("profiles");
+
+            // find one document with new Document
+            Document user1 = profilesCollection.find().first();
+            System.out.println("User1: " + user1.toJson());
+
+            // find one document with Filters.eq() for fullName == "Test Test"
+            Document test = profilesCollection.find(eq("full_name", "Test Test")).first();
+            System.out.println("Test User: " + test.toJson());
+
+            // find a list of documents and iterate throw it using an iterator.
+            // FindIterable<Document> iterable = profilesCollection.find(gte("student_id", 10000));
+            // MongoCursor<Document> cursor = iterable.iterator();
+            // System.out.println("Student list with a cursor: ");
+            // while (cursor.hasNext()) {
+            //     System.out.println(cursor.next().toJson());
+            // }
+
+            // find a list of documents and use a List object instead of an iterator
+            // List<Document> studentList = profilesCollection.find(gte("student_id", 10000)).into(new ArrayList<>());
+            // System.out.println("Student list with an ArrayList:");
+            // for (Document student : studentList) {
+            //     System.out.println(student.toJson());
+            // }
+
+            // find a list of documents and print using a consumer
+            // System.out.println("Student list using a Consumer:");
+            // Consumer<Document> printConsumer = document -> System.out.println(document.toJson());
+            // profilesCollection.find(gte("student_id", 10000)).forEach(printConsumer);
+
+            // find a list of documents with sort and projection
+            List<Document> docs = profilesCollection.find()
+                    .projection(fields(excludeId(), include("username", "email")))
+                    .sort(descending("username"))
+                    .into(new ArrayList<>());
+
+            System.out.println("Users sorted and projected: ");
+            for (Document user : docs) {
+                System.out.println(user.toJson());
+            }
+        }
+    }
+
+    public static Document getByFieldNameAndValue(String db, String collection, String fieldName, Object value) {
+        try (MongoClient mongoClient = MongoClients.create(System.getProperty("mongodb.uri"))) {
+            MongoDatabase DB = mongoClient.getDatabase(db);
+            MongoCollection<Document> Collection = DB.getCollection(collection);
+
+            // find one document with specific fieldName and value
+            Document doc = Collection.find(eq(fieldName, value)).first();
+            return doc;
+        }
+    }
 }
